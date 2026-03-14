a73ceb720157cc3a9379912ffa9d603df792d5e3
Cukurikik <ncsremixindonesia@gmail.com>
2026-03-14 14:00:12 +0700

    update

--- src/app/modules/pdf/shared/components/page-thumbnail-strip/page-thumbnail-strip.component.ts
+++ src/app/modules/pdf/shared/components/page-thumbnail-strip/page-thumbnail-strip.component.ts
@@ -7,9 +7,11 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div class="flex overflow-x-auto gap-4 p-4 bg-white/5 rounded-xl border border-white/10">
-      <div class="text-white/50 text-sm whitespace-nowrap pt-8 px-4" *ngIf="!thumbnails?.length">
-         Thumbnails rendering...
-      </div>
+      @if (!thumbnails.length) {
+        <div class="text-white/50 text-sm whitespace-nowrap pt-8 px-4">
+           Thumbnails rendering...
+        </div>
+      }
       @for (thumb of thumbnails; track thumb) {
          <div class="w-24 shrink-0 aspect-[1/1.414] bg-white rounded flex items-center justify-center text-black font-bold">
            {{thumb}}
@@ -20,7 +22,7 @@ import { CommonModule } from '@angular/common';
 })
 export class PageThumbnailStripComponent {
   @Input() file: File | null = null;
-  @Input() pageCount: number = 0;
+  @Input() pageCount = 0;
   
   // Stubs for now
   get thumbnails(): number[] {
